f695b8b5672f16ea087b59f2ff109d23fc09da08
nilsonsasaki <nilsonsasaki@gmail.com>
2021-04-07 12:35:22 -0300

    Added seven Xilophone keys

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,100 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void xylophoneKey(int keyNumber) {
+    final player = AudioCache();
+    player.play('note$keyNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.red,
+                    child: TextButton(
+                      onPressed: () {
+                        xylophoneKey(1);
+                      },
+                      child: Container(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.orange,
+                    child: TextButton(
+                      onPressed: () {
+                        xylophoneKey(2);
+                      },
+                      child: Container(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.yellow,
+                    child: TextButton(
+                      onPressed: () {
+                        xylophoneKey(3);
+                      },
+                      child: Container(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.green,
+                    child: TextButton(
+                      onPressed: () {
+                        xylophoneKey(4);
+                      },
+                      child: Container(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.blue,
+                    child: TextButton(
+                      onPressed: () {
+                        xylophoneKey(5);
+                      },
+                      child: Container(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.teal,
+                    child: TextButton(
+                      onPressed: () {
+                        xylophoneKey(6);
+                      },
+                      child: Container(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.purple,
+                    child: TextButton(
+                      onPressed: () {
+                        xylophoneKey(7);
+                      },
+                      child: Container(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
